c5f9d693f83fafed752e1db0132fc229317fae83
Andreas <the.hion.v@gmail.com>
2024-08-02 01:15:40 +0200

    add code to be used to make animations work

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,7 +1,14 @@
 package;
 
+import openfl.Lib;
+import openfl.display.BitmapData;
+import openfl.geom.Point;
+import openfl.Assets;
+import engine.SpriteSheet;
+import openfl.display.Bitmap;
 import openfl.display.Sprite;
 import engine.Engine;
+using engine.extensions.BitmapExtensions.BitmapExtender;
 
 class Main extends Sprite
 {
