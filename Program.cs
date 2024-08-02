5051d09cc3f5e27eb41a91a68414bafa8f5965f0
local-interloper <marinoljubetic8@gmail.com>
2024-08-02 12:39:58 +0200

    Added raylib

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Raylib_cs;
+
+namespace HelloWorld;
+
+class Program
+{
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
