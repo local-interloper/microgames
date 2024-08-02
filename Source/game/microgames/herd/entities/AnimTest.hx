7436fda17e5c436dd54f3d9a88d86fd5d2540f55
local-interloper <marinoljubetic8@gmail.com>
2024-08-02 12:36:47 +0200

    Framework switch

--- Source/game/microgames/herd/entities/AnimTest.hx
+++
@@ -1,7 +0,0 @@
-package game.microgames.herd.entities;
-
-import engine.entities.core.AbstractAnimatedSpriteEntity;
-
-class AimTest extends AbstractAnimatedSpriteEntity {
-
-}
--- Source/game/microgames/herd/scenes/HerdScene.hx
+++
@@ -1,7 +0,0 @@
-package game.microgames.herd.scenes;
-
-import engine.entities.core.AbstractScene;
-
-class HerdScene extends AbstractScene {
-
-}
